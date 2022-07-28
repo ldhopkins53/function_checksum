892ed2178d9e942b6d194ca506afe08a4e316a01
l <l>
2022-07-28 22:35:34 +0000

    - update

--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 extern void *main_function_size;
 int main_function_crc __attribute__((section(".main_function_crc"))) = 0;
 
-int __attribute((section(".main_function_section"))) main()
-{
-    printf("testing this one out\n");
-    return EXIT_SUCCESS;
+int __attribute((section(".main_function_section"))) main() {
+  printf("testing this one out\n");
+  return EXIT_SUCCESS;
 }
